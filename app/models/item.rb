2a39f60e126eed3d9084b0df208877e2a145b06d
bignyokki <shiro5989@yahoo.co.jp>
2022-01-29 15:07:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,24 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one_attached :image
-extend ActiveHash::Associations::ActiveRecordExtensions
-belongs_to :item_info
-belongs_to :item_category
-belongs_to :item_sales_status
-belongs_to :item_shipping_fee_status
-belongs_to :prefecture
-belongs_to :item_scheduled_delivery
+  belongs_to :user
+
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_info
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
+
+  
+
+  validates :item_name,                   presence: true
+  validates :item_info,                   presence: true
+  validates :item_category_id,            presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_sales_status_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,               presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_scheduled_delivery_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_price,                  presence: true
 end
