b075ca2f0edeec3863b6157a88b1754765345077
bignyokki <shiro5989@yahoo.co.jp>
2022-01-29 14:48:46 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
 belongs_to :user
 has_one_attached :image
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :item_info
+belongs_to :item_category
+belongs_to :item_sales_status
+belongs_to :item_shipping_fee_status
+belongs_to :prefecture
+belongs_to :item_scheduled_delivery
 end
