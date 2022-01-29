f9ebd0a0d589a1692029932902eb8f773f35ac42
bignyokki <shiro5989@yahoo.co.jp>
2022-01-29 18:08:40 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,5 +19,5 @@ class Item < ApplicationRecord
   validates :item_shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,               presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_scheduled_delivery_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_price,                  presence: true
+  validates :item_price,                  presence: true, numericality: { less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }, format: { with: /\A[0-9]+\z/ }
 end
