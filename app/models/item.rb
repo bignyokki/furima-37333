0ca58b2978adf0b809cd7bebef06cbbdf5ed1b1f
bignyokki <shiro5989@yahoo.co.jp>
2022-01-29 16:31:42 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item_info
   belongs_to :item_category
   belongs_to :item_sales_status
   belongs_to :item_shipping_fee_status
