9f58764849b29d88638f405591529980661dac31
bignyokki <shiro5989@yahoo.co.jp>
2022-02-05 11:41:08 +0900

    共通のコードをメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, except: [:index, :show, :destroy]
   before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @item = Item.order('created_at DESC')
@@ -20,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -30,11 +30,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -64,4 +62,9 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == item.user_id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+    
 end
