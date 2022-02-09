d9b4432e953455ee56fe80deb734a1ccd5e0dc5b
bignyokki <shiro5989@yahoo.co.jp>
2022-02-09 23:41:46 +0900

    テストコードの追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -71,7 +71,17 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include "Item can't be blank"
       end
-
+      it 'priceが空では保存できない' do
+        @order_address.price = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Price can't be blank"
+      end
+      it 'tokenが空では保存できない' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include "Token can't be blank"
+      end
+      
     end
 
   end
